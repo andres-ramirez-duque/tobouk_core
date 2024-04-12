dc03ae1148664392b519e399f35af9cc0d2f4c8b
andres-ramirez-duque <andres.ramirez-duque@glasgow.ac.uk>
2024-04-12 11:45:05 +0000

    dialogs updated and personalitations added to logs

--- src/tobo_core_node.cpp
+++ src/tobo_core_node.cpp
@@ -13,9 +13,21 @@ int main(int argc, char* argv[])
     ros::Rate hz(30);
     std::string logs_path;
     std::string log_name;
+    std::string age;
+    std::string iv;
+    std::string maxselected;
+    std::string reward;
+
     ros::param::get("logs_path", logs_path);
     ros::param::get("core_log", log_name);
-    
+
+    ros::param::get("maxsel", maxselected);
+    ros::param::get("rewa", reward);
+
+    ros::param::get("/parameters/boolean_vars/age", age);
+    ros::param::get("/parameters/boolean_vars/iv", iv);
+
+    std::cout <<" ==== reward ===== " << reward << " ==== maxselected ===== "<< maxselected << "\n";
     ROS_INFO("Initializing the Tobo_core node");
     ToboCore Tobo(_private_node);
     ROS_INFO("Tobo_core is ready");
@@ -27,6 +39,13 @@ int main(int argc, char* argv[])
       std::cout <<"-- Number of Action " << itr->first.c_str() <<" executed: "<< itr->second <<"\n";
       Tobo.logStream << "@[" << currentDateTime() << "]@ -- Number of Action " << itr->first.c_str() <<" executed: "<< itr->second << std::endl;
     }
+
+    Tobo.logStream << "@[" << currentDateTime() << "]@ Personalisation Form: " << std::endl;
+    Tobo.logStream << "@[" << currentDateTime() << "]@ Age: " << age.c_str() << std::endl;
+    Tobo.logStream << "@[" << currentDateTime() << "]@ IV: " << iv.c_str() << std::endl;
+    Tobo.logStream << "@[" << currentDateTime() << "]@ MaxSelected: " << maxselected.c_str() << std::endl;
+    Tobo.logStream << "@[" << currentDateTime() << "]@ Reward: " << reward.c_str() << std::endl;
+
     std::string logContent = Tobo.logStream.str();
     std::cout << logs_path +"/"+ log_name << std::endl;
     std::ofstream logFile(logs_path +"/"+ log_name);
